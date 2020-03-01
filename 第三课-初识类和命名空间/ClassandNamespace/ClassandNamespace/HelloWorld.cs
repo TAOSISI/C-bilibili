78d340c98208e46fe0ddb51cf0f2877db39cce78
TAOSISI <411419750@qq.com>
2020-03-01 13:45:52 +0800

    提交第三课

---
+++ 第三课-初识类和命名空间/ClassandNamespace/ClassandNamespace/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace ClassandNamespace
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            double result = Calculator.Mul(1, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 第三课-初识类和命名空间/MyLibrary/MyLibrary/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    public class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Sub(double a, double b)
+        {
+            return a - b - 10;
+        }
+    }
+}
