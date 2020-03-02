f5c9afd89390a688c0a7c61a61705cc487c782d1
TAOSISI <411419750@qq.com>
2020-03-02 13:11:14 +0800

    上传第五课

---
+++ 第五课-基本元素，类型变量与方法算法/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdentifiersExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 100;
+            long y = 3L;
+            float a = 3.0F;
+            double b = 4.00;
+            char c = 'a';//单引号只能赋值一个字符
+            bool d = true;
+            bool e = false;
+            string str = null;
+            //Form f = null;
+            Form f = new Form();
+            f.ShowDialog();
+            //以var声明的变量，编译器会自动推断出他的类型
+            var x = 3;
+            Console.WriteLine(x.GetType().Name);
+            Console.WriteLine(X);
+            Calculator g = new Calculator();
+            int h = g.Add(2, 3);
+            Console.WriteLine(h);
+            string i = g.Today();
+            Console.WriteLine(i);
+            g.PrintSum(6, 4);
+        }
+        class Calculator
+        {
+            public int Add(int a, int b)
+            {
+                int result = a + b;
+                return result;
+            }
+            public string Today()
+            {
+                int day = DateTime.Now.Day;
+                return day.ToString();
+            }
+            public void PrintSum(int a,int b)
+            {
+                //没有任何数据返回
+                int result = a + b;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ 第五课-基本元素，类型变量与方法算法/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExample
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            //c.PrintXTo1(3);
+            int result = c.SumFrom1ToX(100);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+    class Calculator
+    {
+        //public void PrintXTo1(int x)
+        //{
+        //    for (int i = x; i > 0; i--)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+        //public void PrintXTo1(int x)
+        //{
+        //    if (x == 1)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(x);
+        //        PrintXTo1(x - 1);
+        //    }
+        //}
+        //public int SumFrom1ToX(int X)
+        //{
+        //    int result = 0;
+        //    for (int i = 1; i < X+1; i++)
+        //    {
+        //        result = result + i;
+        //    }
+        //    return result;
+        //}
+        public int SumFrom1ToX(int X)
+        {
+            if (X == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int result = X + SumFrom1ToX(X - 1);
+                return result;
+            }
+        }
+    }
+}
